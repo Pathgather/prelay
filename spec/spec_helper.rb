642818d03ce133dd3c0f76936ce5f4053e3badaf
Chris Hanks <christopher.m.hanks@gmail.com>
2015-12-28 17:45:26 -0500

    Populate more data to play with.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,8 @@ require 'faker'
 
 DB = Sequel.connect(ENV['DATABASE_URL'] || 'postgres:///prelay-test')
 
+DB.extension :pg_json
+
 DB.run <<-SQL
   CREATE EXTENSION IF NOT EXISTS "uuid-ossp" SCHEMA pg_catalog;
 SQL
@@ -18,7 +20,14 @@ DB.drop_table? :publishers, :tracks, :albums, :artists
 DB.create_table :artists do
   uuid :id, primary_key: true, default: Sequel.function(:uuid_generate_v4)
 
-  text :name, null: false
+  text        :name,       null: false
+  integer     :upvotes,    null: false
+  boolean     :active,     null: false
+  float       :popularity, null: false
+  date        :birth_date, null: false
+  numeric     :money_made, null: false
+  jsonb       :other,      null: false
+  timestamptz :created_at, null: false
 end
 
 DB.create_table :albums do
@@ -26,9 +35,16 @@ DB.create_table :albums do
 
   uuid :artist_id, null: false
 
-  foreign_key [:artist_id], :artists
+  text        :name,         null: false
+  integer     :upvotes,      null: false
+  boolean     :high_quality, null: false
+  float       :popularity,   null: false
+  date        :release_date, null: false
+  numeric     :money_made,   null: false
+  jsonb       :other,        null: false
+  timestamptz :created_at,   null: false
 
-  text :name, null: false
+  foreign_key [:artist_id], :artists
 end
 
 DB.create_table :tracks do
@@ -36,10 +52,17 @@ DB.create_table :tracks do
 
   uuid :album_id, null: false
 
-  foreign_key [:album_id], :albums
+  text        :name,         null: false
+  integer     :number,       null: false
+  boolean     :high_quality, null: false
+  float       :popularity,   null: false
+  date        :single_date,  null: false
+  numeric     :money_made,   null: false
+  jsonb       :other,        null: false
+  timestamptz :created_at,   null: false
 
-  text :name, null: false
-  integer :number, null: false
+  unique [:album_id, :number]
+  foreign_key [:album_id], :albums
 end
 
 DB.create_table :publishers do
@@ -47,9 +70,9 @@ DB.create_table :publishers do
 
   uuid :album_id, null: false
 
-  foreign_key [:album_id], :albums
-
   text :name, null: false
+
+  foreign_key [:album_id], :albums
 
   unique :album_id
 end
@@ -87,38 +110,87 @@ end
 
 DB.loggers << logger
 
-artist_ids = DB[:artists].import(
-  [:name],
+def random_json_value
+  case rand
+  when 0.00..0.30 then rand 100000                             # Integer
+  when 0.30..0.60 then Faker::Lorem.sentence                   # String
+  when 0.60..0.70 then rand > 0.5                              # Boolean
+  when 0.70..0.80 then rand * 10000                            # Float
+  when 0.80..0.90 then nil                                     # Null
+  when 0.90..0.95 then rand(4).times.map { random_json_value } # Array (uncommon)
+  when 0.95..1.00 then random_json_doc                         # Doc (uncommon)
+  else raise "Oops!"
+  end
+end
+
+def random_json_doc
+  output = {}
+  Faker::Lorem.words(rand(6)).each do |word|
+    output[word.downcase] = random_json_value
+  end
+  output
+end
+
+artist_ids = DB[:artists].multi_insert(
   25.times.map {
-    [Faker::Name.name]
+    {
+      name:       Faker::Name.name,
+      upvotes:    rand(10000),
+      active:     rand > 0.5,
+      popularity: rand,
+      birth_date: Date.today - (7200 + rand(20000)),
+      money_made: (rand * 1000000).round(2),
+      other:      Sequel.pg_jsonb(random_json_doc),
+      created_at: Time.now - (rand(1000) * 24 * 60 * 60),
+    }
   },
   return: :primary_key
 )
 
-album_ids = DB[:albums].import(
-  [:artist_id, :name],
-  artist_ids.map { |a_id|
+album_ids = DB[:albums].multi_insert(
+  artist_ids.map { |artist_id|
     10.times.map {
-      [a_id, Faker::Lorem.sentence]
+      {
+        artist_id:    artist_id,
+        name:         Faker::Lorem.sentence,
+        upvotes:      rand(10000),
+        high_quality: rand > 0.9,
+        popularity:   rand,
+        release_date: Date.today - rand(1000),
+        money_made:   (rand * 100000).round(2),
+        other:        Sequel.pg_jsonb(random_json_doc),
+        created_at:   Time.now - (rand(1000) * 24 * 60 * 60),
+      }
     }
   }.flatten(1),
   return: :primary_key
 )
 
-track_ids = DB[:tracks].import(
-  [:album_id, :name, :number],
-  album_ids.map { |a_id|
+track_ids = DB[:tracks].multi_insert(
+  album_ids.map { |album_id|
     10.times.map { |i|
-      [a_id, Faker::Lorem.sentence, i + 1]
+      {
+        album_id:     album_id,
+        name:         Faker::Lorem.sentence,
+        number:       i + 1,
+        high_quality: rand > 0.9,
+        popularity:   rand,
+        single_date:  Date.today - rand(1000),
+        money_made:   (rand * 10000).round(2),
+        other:        Sequel.pg_jsonb(random_json_doc),
+        created_at:   Time.now - (rand(1000) * 24 * 60 * 60),
+      }
     }
   }.flatten(1),
   return: :primary_key
 )
 
-publisher_ids = DB[:publishers].import(
-  [:album_id, :name],
-  album_ids.map { |a_id|
-    [a_id, Faker::Company.name]
+publisher_ids = DB[:publishers].multi_insert(
+  album_ids.map { |album_id|
+    {
+      album_id: album_id,
+      name:     Faker::Company.name
+    }
   },
   return: :primary_key
 )
