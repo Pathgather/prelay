c333d0153f025ab4b1c5c5faffa11cfe1de65dd5
Chris Hanks <christopher.m.hanks@gmail.com>
2016-01-29 15:36:20 -0500

    Minor spec reorganizing.

--- spec/unit/prelay_gem_spec.rb
+++ spec/unit/prelay_gem_spec.rb
@@ -3,6 +3,8 @@
 require 'spec_helper'
 
 class PrelayGemSpec < PrelaySpec
+  # Silly spec, but make sure our frozen string literal magic comments are
+  # being taken seriously :)
   it "should have a frozen version number" do
     assert_instance_of String, ::Prelay::VERSION
     assert ::Prelay::VERSION.frozen?
--- spec/unit/type_graphql_object_spec.rb
+++ spec/unit/type_graphql_object_spec.rb
@@ -2,7 +2,7 @@
 
 require 'spec_helper'
 
-class GraphQLObjectSpec < PrelaySpec
+class TypeGraphQLObjectSpec < PrelaySpec
   describe "for a type" do
     let(:object) { AlbumType.graphql_object }
 
@@ -47,53 +47,6 @@ class GraphQLObjectSpec < PrelaySpec
       assert_equal 'publisher', field.name
       assert_equal 'Publisher', field.type.to_s
       assert_equal "The publisher responsible for releasing the album.", field.description
-    end
-  end
-
-  describe "for an interface" do
-    let(:object) { ReleaseInterface.graphql_object }
-
-    it "should translate it to a GraphQL object" do
-      assert_instance_of GraphQL::InterfaceType, object
-      assert_equal 'Release', object.name
-      assert_equal ['id', 'name', 'upvotes', 'high_quality', 'popularity', 'artist', 'tracks', 'publisher'], object.fields.keys
-      assert_equal "A collection of songs released by an artist.", object.description
-    end
-
-    it "should translate its attributes to GraphQL fields" do
-      field = object.fields['name']
-
-      assert_instance_of GraphQL::Field, field
-      assert_equal 'name', field.name
-      assert_equal 'String!', field.type.to_s
-      assert_equal "The name of the release", field.description
-    end
-
-    it "should translate its many_to_one associations to GraphQL fields" do
-      field = object.fields['artist']
-
-      assert_instance_of GraphQL::Field, field
-      assert_equal 'artist', field.name
-      assert_equal 'Artist!', field.type.to_s
-      assert_equal "The artist who released the release.", field.description
-    end
-
-    it "should translate its one_to_many associations to GraphQL connections" do
-      field = object.fields['tracks']
-
-      assert_instance_of GraphQL::Field, field
-      assert_equal 'tracks', field.name
-      assert_equal 'TrackConnection', field.type.to_s
-      assert_equal "The tracks on this release.", field.description
-    end
-
-    it "should translate its one_to_one associations to GraphQL fields" do
-      field = object.fields['publisher']
-
-      assert_instance_of GraphQL::Field, field
-      assert_equal 'publisher', field.name
-      assert_equal 'Publisher', field.type.to_s
-      assert_equal "The publisher responsible for releasing the release.", field.description
     end
   end
 end
