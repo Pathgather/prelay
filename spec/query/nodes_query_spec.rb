0da8d410e54463f8ce5a1db525142e7a70cb8f2a
Chris Hanks <christopher.m.hanks@gmail.com>
2016-01-29 15:42:36 -0500

    Fix up NodesQuerySpec.

--- spec/query/nodes_query_spec.rb
+++ spec/query/nodes_query_spec.rb
@@ -3,11 +3,11 @@
 require 'spec_helper'
 
 class NodesQuerySpec < PrelaySpec
-  let(:album)  { Album.first }
-  let(:track)  { Track.first }
+  let(:album)  { Album.first  }
+  let(:track)  { Track.first  }
   let(:artist) { Artist.first }
 
-  it "should support refetching an item by its relay id" do
+  it "should support refetching multiple nodes by their relay ids" do
     id1 = encode 'Album',  album.id
     id2 = encode 'Track',  track.id
     id3 = encode 'Artist', artist.id
@@ -28,22 +28,22 @@ class NodesQuerySpec < PrelaySpec
       'data' => {
         'nodes' => [
           {
-            'id' => id1,
-            '__typename' => "Album",
-            'name' => album.name,
+            'id'           => id1,
+            '__typename'   => "Album",
+            'name'         => album.name,
             'high_quality' => album.high_quality,
           },
           {
-            'id' => id2,
-            '__typename' => "Track",
-            'name' => track.name,
-            'number' => track.number,
+            'id'           => id2,
+            '__typename'   => "Track",
+            'name'         => track.name,
+            'number'       => track.number,
           },
           {
-            'id' => id3,
-            '__typename' => "Artist",
-            'name' => artist.name,
-            'upvotes' => artist.upvotes,
+            'id'           => id3,
+            '__typename'   => "Artist",
+            'name'         => artist.name,
+            'upvotes'      => artist.upvotes,
           },
         ]
       }
@@ -53,5 +53,66 @@ class NodesQuerySpec < PrelaySpec
       %(SELECT "tracks"."id", "tracks"."name", "tracks"."number" FROM "tracks" WHERE ("tracks"."id" = '#{track.id}') ORDER BY "tracks"."id"),
       %(SELECT "artists"."id", "artists"."first_name", "artists"."last_name", "artists"."upvotes" FROM "artists" WHERE ("artists"."id" = '#{artist.id}') ORDER BY "artists"."id"),
     ]
+  end
+
+  it "should handle interfaces in a nodes query" do
+    # Once this is fixed it can replace the above one.
+    skip "File an issue with the GraphQL gem?"
+
+    id1 = encode 'Album',  album.id
+    id2 = encode 'Track',  track.id
+    id3 = encode 'Artist', artist.id
+
+    execute_query <<-GRAPHQL
+      query Query {
+        nodes(ids: ["#{id1}", "#{id2}", "#{id3}"]) {
+          id,
+          __typename,
+          ... on Album   { name, high_quality }
+          ... on Artist  { name, upvotes }
+          ... on Track   { name, number }
+          ... on Release { name, popularity }
+        }
+      }
+    GRAPHQL
+
+    assert_result \
+      'data' => {
+        'nodes' => [
+          {
+            'id'           => id1,
+            '__typename'   => "Album",
+            'name'         => album.name,
+            'high_quality' => album.high_quality,
+            'popularity'   => album.popularity,
+          },
+          {
+            'id'           => id2,
+            '__typename'   => "Track",
+            'name'         => track.name,
+            'number'       => track.number,
+          },
+          {
+            'id'           => id3,
+            '__typename'   => "Artist",
+            'name'         => artist.name,
+            'upvotes'      => artist.upvotes,
+          },
+        ]
+      }
+
+    assert_sqls [
+      %(SELECT "albums"."id", "albums"."name", "albums"."high_quality" FROM "albums" WHERE ("albums"."id" = '#{album.id}') ORDER BY "albums"."id"),
+      %(SELECT "tracks"."id", "tracks"."name", "tracks"."number" FROM "tracks" WHERE ("tracks"."id" = '#{track.id}') ORDER BY "tracks"."id"),
+      %(SELECT "artists"."id", "artists"."first_name", "artists"."last_name", "artists"."upvotes" FROM "artists" WHERE ("artists"."id" = '#{artist.id}') ORDER BY "artists"."id"),
+    ]
+  end
+
+  it "when a record no longer exists should return an array containing NULL" do
+    skip "Doesn't seem to be supported by the GraphQL gem? It wants a type for NULL."
+  end
+
+  it "should retrieve multiple records of the same type in the same query" do
+    skip "Optimization"
   end
 end
