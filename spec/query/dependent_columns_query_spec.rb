8b7a54865f6db63f2544dc962546132628badcc4
Chris Hanks <christopher.m.hanks@gmail.com>
2016-02-22 15:53:26 -0500

    Spec that the same column isn't loaded multiple times even if the dependencies are duplicative.

--- spec/query/dependent_columns_query_spec.rb
+++ spec/query/dependent_columns_query_spec.rb
@@ -14,6 +14,7 @@ class DependentColumnsQuerySpec < PrelaySpec
       description "A musician"
 
       attribute :name, "The full name of the artist", datatype: :string, dependent_columns: [:first_name, :last_name]
+      attribute :first_name, "The first name of the artist", datatype: :string
       attribute :upvotes, "How many upvotes the artist got", datatype: :integer
 
       def name
@@ -50,5 +51,17 @@ class DependentColumnsQuerySpec < PrelaySpec
     assert_sqls [
       %(SELECT "artists"."id", "artists"."first_name", "artists"."last_name", "artists"."upvotes" FROM "artists" WHERE ("artists"."id" = '#{artist.id}'))
     ]
+  end
+
+  it "should not try to load the same column twice, even when the dependencies are duplicative" do
+    id = id_for(artist)
+
+    execute_query "query Query { node(id: \"#{id}\") { id, ... on Artist { first_name, name, upvotes } } }"
+    assert_result 'data' => { 'node' => { 'id' => id, 'first_name' => artist.first_name, 'name' => artist.first_name + ' ' + artist.last_name, 'upvotes' => artist.upvotes } }
+    assert_sqls [%(SELECT "artists"."id", "artists"."first_name", "artists"."last_name", "artists"."upvotes" FROM "artists" WHERE ("artists"."id" = '#{artist.id}'))]
+
+    execute_query "query Query { node(id: \"#{id}\") { id, ... on Artist { name, first_name, upvotes } } }"
+    assert_result 'data' => { 'node' => { 'id' => id, 'first_name' => artist.first_name, 'name' => artist.first_name + ' ' + artist.last_name, 'upvotes' => artist.upvotes } }
+    assert_sqls [%(SELECT "artists"."id", "artists"."first_name", "artists"."last_name", "artists"."upvotes" FROM "artists" WHERE ("artists"."id" = '#{artist.id}'))]
   end
 end
