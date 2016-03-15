5927876806795e1495b12ec999f6220985c09ed1
Chris Hanks <christopher.m.hanks@gmail.com>
2016-03-15 14:54:01 -0400

    Support input timestamps.

--- lib/prelay/time_type.rb
+++ lib/prelay/time_type.rb
@@ -9,10 +9,7 @@ module Prelay
     name "Timestamp"
     description "Time and date in ISO 8601"
 
-    coerce_input ->(value) {
-      # Looks like if the client is fine with sending it, we can use Time.iso8601(value).
-      raise Error, "Input syntax for Time objects not finalized yet"
-    }
+    coerce_input ->(value) { Time.iso8601(value) }
     coerce_result ->(value) { value.iso8601 }
   end
 end
