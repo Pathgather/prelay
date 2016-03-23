3e7fea1e946f8c9fa22cf0726110dabc9f487bad
Chris Hanks <christopher.m.hanks@gmail.com>
2016-03-23 14:56:23 -0400

    Output timestamps in microseconds, to stick to Postgres' level of precision.

--- lib/prelay/time_type.rb
+++ lib/prelay/time_type.rb
@@ -12,6 +12,6 @@ module Prelay
     # Special-case empty strings as a way to pass null values to the server,
     # though the GraphQL gem won't actually let us return nil :(
     coerce_input ->(value) { value == '' ? '' : Time.iso8601(value) }
-    coerce_result ->(value) { value.iso8601 }
+    coerce_result ->(value) { value.iso8601(6) }
   end
 end
