ef25e20daa46e419af3ba2ec0c84d602b981acc9
Chris Hanks <christopher.m.hanks@gmail.com>
2016-03-24 13:52:53 -0400

    Add a pending spec for limiting queries on associations to interfaces to just the relevant target types.

--- spec/integration/type/associations/one_to_many/interface_association_spec.rb
+++ spec/integration/type/associations/one_to_many/interface_association_spec.rb
@@ -75,5 +75,67 @@ class OneToManyInterfaceAssociationSpec < PrelaySpec
     ]
   end
 
+  it "should support fetching associated items through a one-to-many association to an interface when only a subset of types are necessary" do
+    skip
+
+    assert_instance_of Prelay::Type::Association, ArtistType.associations.delete(:releases)
+    ArtistType.one_to_many :releases, "Albums and Compilations released by the artist", order: :created_at, target: ReleaseInterface, foreign_key: :artist_id, target_types: [AlbumType]
+
+    artist = Artist.first!
+
+    id = id_for(artist)
+
+    execute_query <<-GRAPHQL
+      query Query {
+        node(id: "#{id}") {
+          id,
+          ... on Artist {
+            first_name,
+            releases(first: 200) {
+              edges {
+                node {
+                  __typename,
+                  id,
+                  upvotes,
+                  ... on Album {
+                    name
+                  },
+                  ... on Compilation {
+                    high_quality
+                  },
+                }
+              }
+            }
+          }
+        }
+      }
+    GRAPHQL
+
+    assert_result \
+      'data' => {
+        'node' => {
+          'id' => id,
+          'first_name' => artist.first_name,
+          'releases' => {
+            'edges' => artist.albums.map { |a|
+              {
+                'node' => {
+                  '__typename' => "Album",
+                  'id' => id_for(a),
+                  'upvotes' => a.upvotes,
+                  'name' => a.name,
+                }
+              }
+            }
+          }
+        }
+      }
+
+    assert_sqls [
+      %(SELECT "artists"."id", "artists"."first_name" FROM "artists" WHERE ("artists"."id" = '#{artist.id}')),
+      %(SELECT "albums"."id", "albums"."name", "albums"."upvotes", "albums"."artist_id", "albums"."created_at" AS "cursor" FROM "albums" WHERE ("albums"."artist_id" IN ('#{artist.id}')) ORDER BY "created_at" LIMIT 200),
+    ]
+  end
+
   it "should limit items appropriately"
 end
