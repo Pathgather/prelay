70a52896e5ba34b94ceea1c0dc5105d6192d7277
Chris Hanks <christopher.m.hanks@gmail.com>
2016-04-13 11:29:35 -0400

    Throw together pagination specs for top-level connections.

--- spec/integration/pagination_spec.rb
+++ spec/integration/pagination_spec.rb
@@ -9,6 +9,20 @@ class PaginationSpec < PrelaySpec
     let(:releases) { artist.releases }
 
     describe "against a type" do
+      mock_schema do
+        query :Albums do
+          include Prelay::Connection
+          type AlbumType
+          order Sequel.asc(:created_at)
+        end
+
+        query :Releases do
+          include Prelay::Connection
+          type ReleaseInterface
+          order Sequel.asc(:created_at)
+        end
+      end
+
       it "should raise an error if a 'first' or 'last' argument is not passed" do
         artist_id = id_for(artist)
 
@@ -122,6 +136,65 @@ class PaginationSpec < PrelaySpec
                           'node' => {
                             'id' => id_for(artist),
                             'first_name' => artist.first_name,
+                            (paginating_through_interface ? 'releases' : 'albums') => expectation,
+                          }
+                        }
+                    end
+
+                    it "on a top-level connection query should support #{desc}" do
+                      # TODO: Hacky, clean up.
+                      Track.dataset.delete
+                      Publisher.dataset.delete
+                      Album.exclude(artist_id: artist.id).delete
+                      Compilation.exclude(artist_id: artist.id).delete
+
+                      all_albums      = paginating_through_interface ? releases : albums
+                      expected_albums = all_albums
+                      expected_albums = expected_albums.reverse unless paginating_forward
+
+                      args = {}
+                      args[paginating_forward ? :first : :last  ] = all_records_requested ? all_albums.length : 3
+                      args[paginating_forward ? :after : :before] = to_cursor(expected_albums[1].created_at) if cursor_passed
+
+                      expected_albums = expected_albums[2..-1] if cursor_passed
+                      expected_albums = expected_albums[0..2] unless all_records_requested
+                      expected_albums = expected_albums.reverse unless paginating_forward
+
+                      graphql =
+                        <<-GRAPHQL
+                          query Query {
+                            connections {
+                              #{paginating_through_interface ? 'releases' : 'albums'}(#{graphql_args(args)}) {
+                                # count
+                                edges {
+                                  #{'cursor,' if cursor_requested}
+                                  node {
+                                    id,
+                                    name
+                                  }
+                                }
+                                #{page_info_query}
+                              }
+                            }
+                          }
+                        GRAPHQL
+
+                      execute_query(graphql)
+
+                      expectation = {
+                        # 'count' => all_albums.length,
+                        'edges' => expected_albums.map { |a|
+                          h = {'node' => {'id' => id_for(a), 'name' => a.name}}
+                          h['cursor'] = to_cursor(a.created_at) if cursor_requested
+                          h
+                        }
+                      }
+
+                      expectation['pageInfo'] = page_info if page_info
+
+                      assert_result \
+                        'data' => {
+                          'connections' => {
                             (paginating_through_interface ? 'releases' : 'albums') => expectation,
                           }
                         }
