79f5496368f7506a8e1137ab72d63144e2717ba4
Chris Hanks <christopher.m.hanks@gmail.com>
2016-04-29 19:17:22 -0400

    Implement another mutation spec.

--- spec/integration/mutation/sequenced_mutation_spec.rb
+++ spec/integration/mutation/sequenced_mutation_spec.rb
@@ -3,5 +3,59 @@
 require 'spec_helper'
 
 class SequencedMutationSpec < PrelaySpec
-  it "should run mutations in the passed order"
+  mock_schema do
+    mutation :AppendToAlbumName do
+      type AlbumType
+
+      argument :id, :id
+      argument :str, :string
+
+      result_field :album, association: :self
+
+      def mutate(id:, str:)
+        album = Prelay::ID.parse(id, schema: self.class.schema).get
+        album.update(name: album.name + str)
+        {album: album.id}
+      end
+    end
+  end
+
+  it "should run mutations in the passed order" do
+    album = Album.first
+    id = id_for(album)
+    album.update name: 'a'
+
+    execute_query <<-GRAPHQL
+      mutation Mutation {
+        append_a: append_to_album_name(input: {id: "#{id}", str: "b", clientMutationId: "b"}) {
+          clientMutationId,
+          album { id, name }
+        }
+        append_b: append_to_album_name(input: {id: "#{id}", str: "c", clientMutationId: "c"}) {
+          clientMutationId,
+          album { id, name }
+        }
+      }
+    GRAPHQL
+
+    assert_equal 'abc', album.reload.name
+
+    assert_result \
+      'data' => {
+        'append_a' => {
+          'album' => {
+            'id' => id,
+            'name' => 'ab',
+          },
+          'clientMutationId' => 'b',
+        },
+        'append_b' => {
+          'album' => {
+            'id' => id,
+            'name' => 'abc',
+          },
+          'clientMutationId' => 'c',
+        },
+      }
+  end
 end
